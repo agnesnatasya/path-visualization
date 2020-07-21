605de43c6246a8aefc7a50a3898642506d76e1dd
agnesnatasya <agnesnatasyawijaya@gmail.com>
2020-07-22 02:24:42 +0800

    Handle wall dragging, moving start and end button

--- src/components/Grids/Grids.js
+++ src/components/Grids/Grids.js
@@ -9,18 +9,22 @@ export default class Grids extends Component {
     this.state = {
       rowSize: 25,
       colSize: 50,
-      startRow: 13,
+      startRow: 12,
       startCol: 15,
-      endRow: 13,
+      endRow: 12,
       endCol: 35,
       grids: [],
+      mousePressed: false,
+      buttonDragged: null,
     }
+    this.handleMouseDown = this.handleMouseDown.bind(this);
+    this.handleMouseEnter = this.handleMouseEnter.bind(this);
+    this.handleMouseUp = this.handleMouseUp.bind(this);
+    this.handleMouseLeave = this.handleMouseLeave.bind(this);
   }
 
   componentDidMount() {
-    this.setState(prevState => {
-      return { grids: this.setInitialGrids() };
-    });
+    this.setState({ grids: this.setInitialGrids() });
   }
 
   setInitialGrids() {
@@ -35,15 +39,90 @@ export default class Grids extends Component {
     return initialGrids;
   }
 
+  isStartGrid(row, col) {
+    return (row === this.state.startRow) && (col === this.state.startCol);
+  }
+
+  isEndGrid(row, col) {
+    return (row === this.state.endRow) && (col === this.state.endCol)
+  }
+
   initialGrid(row, col) {
-    if ((row === this.state.endRow) && (col === this.state.endCol)) {
-      console.log(true)
-    }
     return {
       isWallGrid: false,
-      isStartGrid: (row === this.state.startRow) && (col === this.state.startCol),
-      isEndGrid: (row === this.state.endRow) && (col === this.state.endCol),
+      isStartGrid: this.isStartGrid(row, col),
+      isEndGrid: this.isEndGrid(row, col),
     };
+  }
+
+  toggleWallGrid(row, col) {
+    this.setState(prevState => {
+      const newGrids = prevState.grids.slice();
+      const oldGrid = newGrids[row][col];
+      const newGrid = Object.assign({}, oldGrid, { isWallGrid: !oldGrid.isWallGrid });
+      newGrids[row][col] = newGrid;
+      return ({ grids: newGrids, mousePressed: true });
+    })
+  }
+
+  toggleStartEndPosition(row, col) {
+    this.setState(prevState => {
+      const newGrids = prevState.grids.slice();
+      const oldGrid = newGrids[row][col];
+      if (this.state.buttonDragged === 'start') {
+        const newGrid = Object.assign({}, oldGrid, { isStartGrid: !oldGrid.isStartGrid, isWallGrid: false });
+        newGrids[row][col] = newGrid;
+        return ({ grids: newGrids, startRow: row, startCol: col });
+      } else {
+        const newGrid = Object.assign({}, oldGrid, { isEndGrid: !oldGrid.isEndGrid, isWallGrid: false });
+        newGrids[row][col] = newGrid;
+        return ({ grids: newGrids, endRow: row, endCol: col });
+      }
+    });
+  }
+
+  handleMouseDown(row, col) {
+    if (this.isStartGrid(row,col)) {
+      this.setState({buttonDragged: 'start', mousePressed: true });
+    } else if (this.isEndGrid(row, col)) {
+      this.setState({ buttonDragged: 'end', mousePressed: true });
+    } else {
+      this.toggleWallGrid(row, col);
+    }
+  }
+
+  handleMouseEnter(row, col) {
+    if (this.state.buttonDragged) {
+      this.toggleStartEndPosition(row, col);
+    } else if (this.state.mousePressed) {
+      if (this.isStartGrid(row, col) || this.isEndGrid(row, col)) {
+        return;
+      }
+      this.toggleWallGrid(row, col);
+    }
+  }
+
+  handleMouseLeave(row, col) {
+    if (this.state.buttonDragged) {
+      /*this.setState(prevState => {
+        const newGrids = prevState.grids.slice();
+        const oldGrid = newGrids[row][col];
+        if (this.state.buttonDragged === 'start') {
+          const newGrid = Object.assign({}, oldGrid, { isStartGrid: false });
+          newGrids[row][col] = newGrid;
+          return ({ grids: newGrids });
+        } else {
+          const newGrid = Object.assign({}, oldGrid, { isEndGrid: false });
+          newGrids[row][col] = newGrid;
+          return ({ grids: newGrids });
+        }
+      });*/
+      this.toggleStartEndPosition(row, col);
+    }
+  }
+
+  handleMouseUp(row, col) {
+    this.setState({ mousePressed: false, buttonDragged: null })
   }
 
   render() {
@@ -55,12 +134,15 @@ export default class Grids extends Component {
                 <tr key={rowIndex}>
                   {row.map((col, colIndex) => {
                     var { isWallGrid, isStartGrid, isEndGrid } = col;
-                    if (isEndGrid) {
-                      console.log("THIS IS END")
-                    }
                       return (
                         <Grid
                           key={colIndex}
+                          row={rowIndex}
+                          col={colIndex}
+                          onMouseDown={this.handleMouseDown}
+                          onMouseUp={this.handleMouseUp}
+                          onMouseEnter={this.handleMouseEnter}
+                          onMouseLeave={this.handleMouseLeave}
                           isWallGrid={isWallGrid}
                           isStartGrid={isStartGrid}
                           isEndGrid={isEndGrid}
