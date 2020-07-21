96e1fe0c6bb7fd83b1a14c9955ffd15f493cb052
agnesnatasya <agnesnatasyawijaya@gmail.com>
2020-07-21 17:57:03 +0800

    set class names of the grid according to props

--- src/components/Grids/Grid.js
+++ src/components/Grids/Grid.js
@@ -1,20 +1,24 @@
 import React, { Component } from 'react';
 
+import './Grid.css';
+
 export default class Grid extends Component {
   constructor(props) {
     super(props);   
   }
 
-  getAdditionalGridProperties() {
+  getAdditionalGridProps() {
     var additionalGridProperties = '';
-    this.props.isStartGrid ? additionalGridProperties += 'grid-start' : null;
-    this.props.isEndGrid ? additionalGridProperties += 'grid-end' : null;
-    this.props.isWallGrid? additionalGridProperties += 'grid-wall' : null;
+    additionalGridProperties += this.props.isStartGrid ? 'grid-start' : '';
+    additionalGridProperties += this.props.isEndGrid ? 'grid-end' : '';
+    additionalGridProperties += this.props.isWallGrid ? 'grid-wall' : '';
+    return additionalGridProperties
   }
+
   render() {
-    var additionalGridProperties = 
     return (
-      <div className={`grid ${}`}>
+      <div className={`grid ${this.getAdditionalGridProps()}`}>
+        a
       </div>
     );
   }
