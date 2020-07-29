cfb90293a09295ba69a60a9ac51bb1cbbff09e89
agnesnatasya <agnesnatasyawijaya@gmail.com>
2020-07-30 00:08:25 +0800

    Add bootstrap library

--- src/App.js
+++ src/App.js
@@ -2,11 +2,13 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 import Grids from './components/Grids/Grids.js'
+import { Navbar, Nav, NavDropdown, Button, Form, Toast } from 'react-bootstrap';
+
 
 function App() {
   return (
     <div className="App">
-      <Grids />
+        <Grids />
     </div>
   );
 }
