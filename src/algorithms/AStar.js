1afcf0c1a547578acf02b6528d0932b7fabfcd63
agnesnatasya <agnesnatasyawijaya@gmail.com>
2020-07-31 03:16:40 +0800

    Fix bugs in AStar

--- src/algorithms/AStar.js
+++ src/algorithms/AStar.js
@@ -1,37 +1,37 @@
 export function astar(startGrid, endGrid, allGrids) {
-
+  var visitedGridsInOrder = []
   initializeProps(allGrids)
   var openList = [];
   var closedList = [];
-  openList.push(start);
+  openList.push(startGrid);
 
   while (openList.length > 0) {
     let lowestF = 0;
     for (let i = 0; i < openList.length; i++) {
-      if (openList[i].f < openList[lowestF].f) { lowestF = i; }
+      if (openList[i].fValue < openList[lowestF].fValue) { lowestF = i; }
     }
     let currentGrid = openList[lowestF];
 
-
+    visitedGridsInOrder.push(currentGrid);
     if (currentGrid.row === endGrid.row && currentGrid.col === endGrid.col) {
-      return [openList, getShortestPath(endGrid)]
+      return [visitedGridsInOrder, getShortestPath(endGrid)]
     }
 
     const indexCurrentGrid = openList.indexOf(currentGrid);
-    if (index > -1) {
+    if (indexCurrentGrid > -1) {
       openList.splice(indexCurrentGrid, 1);
     }
-    closedList.push(currentNode);
+    closedList.push(currentGrid);
     let neighbors = getNeighborsOfGrid(currentGrid, allGrids, closedList);
 
-    for (var i = 0; i < neighbors.length; i++) {
+    for (let i = 0; i < neighbors.length; i++) {
       let neighbor = neighbors[i];
       
       let currentG = currentGrid.gValue + 1;
       let isBestG = false;
 
-      if (!openList.includes(neighbor)) {
-        neighbor.hValue = Math.abs(currentGrid.row - endGrid.row) + Math.abs(currentGrid.col - endGrid.col);
+      if (!isPartOfArray(neighbor, openList)) {
+        neighbor.hValue = Math.abs(neighbor.row - endGrid.row) + Math.abs(neighbor.col - endGrid.col);
         isBestG = true;
         openList.push(neighbor);
       } else if (currentG < neighbor.gValue) {
@@ -63,7 +63,7 @@ function getShortestPath(endGrid) {
   var currentGrid = endGrid;
   while (currentGrid.previousGrid) {
     shortestPath.unshift(currentGrid);
-    curr = curr.previousGrid;
+    currentGrid = currentGrid.previousGrid;
   }
   return shortestPath;
 };
@@ -73,9 +73,11 @@ function getNeighborsOfGrid(grid, allGrids, closedList) {
   var neighborRight = grid.col < allGrids[0].length - 1 ? allGrids[grid.row][grid.col + 1] : null;
   var neighborLeft = grid.col > 0 ? allGrids[grid.row][grid.col - 1] : null;
   var neighborTop = grid.row > 0 ? allGrids[grid.row - 1][grid.col] : null;
-  return [neighborTop, neighborRight, neighborDown, neighborLeft].filter((neighbor) =>
-    neighbor !== null && (!neighbor.isWallGrid && !closedList.includes(neighbor))
+  return [neighborRight, neighborDown, neighborLeft, neighborTop].filter((neighbor) =>
+    neighbor !== null && (!neighbor.isWallGrid && !isPartOfArray(neighbor, closedList))
   );
 }
 
-
+function isPartOfArray(grid, gridsList) {
+  return gridsList.some((item) => (item.row === grid.row && item.col === grid.col));
+}
