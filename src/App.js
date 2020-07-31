83f7a8500eabc7cbfbd47f1f63f00e2f1d08f665
agnesnatasya <agnesnatasyawijaya@gmail.com>
2020-07-31 17:58:46 +0800

    App to rener title, info bar and grids

--- src/App.js
+++ src/App.js
@@ -1,14 +1,16 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
-import Grids from './components/Grids/Grids.js'
-import { Navbar, Nav, NavDropdown, Button, Form, Toast } from 'react-bootstrap';
-
+import { Title } from './components/Title';
+import { InfoBar } from "./components/InfoBar";
+import Grids from "./components/Grids/Grids.js";
 
 function App() {
   return (
     <div className="App">
-        <Grids />
+      <Title />
+      <InfoBar />
+      <Grids />
     </div>
   );
 }
