4463b2e690ca8ea7b6a890cd4caca6078f9685b7
agnesnatasya <agnesnatasyawijaya@gmail.com>
2020-07-31 17:59:39 +0800

    Move title to a new component

---
+++ src/components/Title.js
@@ -0,0 +1,11 @@
+import React from "react";
+import { Navbar } from 'react-bootstrap';
+import "./Title.css";
+
+export function Title() {
+  return (
+    <div className="header-bar">
+      <span>Path Visualization</span>
+    </div>
+  );
+}
