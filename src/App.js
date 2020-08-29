97f50b36864eb10c87b1d4955f17ad609ffab9a5
agnesnatasya <agnesnatasyawijaya@gmail.com>
2020-08-30 02:46:52 +0800

    Move InfoBar component to the Grids

--- src/App.js
+++ src/App.js
@@ -2,14 +2,12 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 import { Title } from './components/Title';
-import { InfoBar } from "./components/InfoBar";
 import Grids from "./components/Grids/Grids.js";
 
 function App() {
   return (
     <div className="App">
       <Title />
-      <InfoBar />
       <Grids />
     </div>
   );
