2bb34be39e1c9d14d4a6d9b5c42a63399424fc3a
agnesnatasya <agnesnatasyawijaya@gmail.com>
2020-10-01 02:40:29 +0800

    Add explanation of how the traversal works in the visualizer

--- src/components/Traversal.js
+++ src/components/Traversal.js
@@ -4,67 +4,25 @@ import "./Traversal.css";
 
 export function Traversal() {
   return (
-    <Container>
+    <Container className="content">
       <Row>
         <Col>
-          <h6>
-            What does the time complexity visualizer actually measure and how does it measures?
-          </h6>
-          <p>
-            In the background, the visualizer actually inputs different values of <pre>n</pre> into the function that is submitted.
-            The visualizer then calculates the lines excecuted by the algorithm from these different <pre>n</pre> sizes.<br />
-            It then does a regression accross these values collected. In other words, it is trying to find the best function that represents <pre>f(n)</pre>.
-            This serves to be an estimation of the tight bound of the algorithm, though it might not be 100% accurate.
-          </p>
+          <h6>How does the visualizer works?</h6>
+          <p></p>
 
           <p>
-            This is why, the visualizer only accepts one input, this input will be the size of n. For functions that have more than one input, no worries!
-             Look at the sample code provided in the visualizer for example to do this!
-          </p>
-
-          <h6>
-            Are we analysing the best, average of worst case of the algorithm?
-          </h6>
-          <p>
-            This acutally depends on the input that is given by the user. As mentioned in the theory explanation, 
-            most of the time, we are interested in the worst-case analysis. How do we achieve this?<br />
-
-            Make sure that the input choice does not cause the algorithm to terminate early. For example, the sample function<br />
-            <code>
-              def f(x):
-              <br />
-              &nbsp;&nbsp;def binarySearch(arr, l, r, x): 
-              <br />
-              &nbsp;&nbsp;&nbsp;&nbsp;if r >= l: 
-              <br />
-              &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;mid = l + (r - l) // 2
-              <br />
-              &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;if arr[mid] == x: 
-              <br />
-              &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;return mid 
-              <br />
-              &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;elif arr[mid] > x: 
-              <br />
-              &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;return binarySearch(arr, mid + 1, r, x) 
-              <br />
-              &nbsp;&nbsp;&nbsp;&nbsp;else:
-              <br />
-              &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;return -1
-              <br />
-              &nbsp;&nbsp;import random
-              <br />
-              &nbsp;&nbsp;for i in range(x):
-              <br />
-              &nbsp;&nbsp;&nbsp;&nbsp;arr = random.sample(range(0, x), x)
-              <br />
-              &nbsp;&nbsp;binarySearch(arr, 0, len(arr)-1, x+1)
-            </code>
-          </p>
-          <p>
-            The code is doing a binary search, it will terminate when it finds the number.<br />
-            How do we make sure that we are analysing the worst-case? Find number that is not in the array.<br />
-            In this case, the list contains number from 0 to <pre>x</pre> and the number searched is <pre>x + 1</pre>. <br />
-            Thus, this is a worst-case analysis.
+            Quite simple actually, the visualizer works just like the pseudocode
+            that was presented in the theory page. It is implemented in
+            JavaScript, and the result of the traversal is the path from the
+            start to the goal node. As can be deduced from the theory page, some
+            of them are optimal, some are not, according to the algorithm used.
+            <br /><br />
+            The optimal path from the start to the goal node is denoted in{" "}
+            <span className="shortest-font">this</span> color.
+            <br />
+            Besides optimal path, we also track the nodes that are visited but are not
+            part of the path from the start to the goal node. They are denoted in{" "}
+            <span className="visited-font">this</span> color.
           </p>
         </Col>
       </Row>
