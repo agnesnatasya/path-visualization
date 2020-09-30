c1d2357e525b26cb36ceb297f85ef1fceacf441a
agnesnatasya <agnesnatasyawijaya@gmail.com>
2020-09-30 21:49:47 +0800

    Add routing for different pages

--- src/App.js
+++ src/App.js
@@ -1,16 +1,92 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
-import { Title } from './components/Title';
-import Grids from "./components/Grids/Grids.js";
+import { Theory } from "./components/Theory";
+import { Grids } from "./components/Regression";
+import { CodeForm } from './components/CodeForm';
+import { BrowserRouter as Router, Switch, Route, NavLink } from "react-router-dom";
+import { Nav } from "react-bootstrap";
 
-function App() {
-  return (
-    <div className="App">
-      <Title />
-      <Grids />
-    </div>
-  );
+export default function App() {
+    return (
+        <Router>
+            <div>
+                <nav className="navbar navbar-expand-sm navbar-light bg-custom header-bar">
+                    <button
+                        className="navbar-toggler"
+                        type="button"
+                        data-toggle="collapse"
+                        data-target="#navbarNav"
+                        aria-controls="navbarNav"
+                        aria-expanded="false"
+                        aria-label="Toggle navigation"
+                    >
+                        <span className="navbar-toggler-icon"></span>
+                    </button>
+                    <div className="collapse navbar-collapse" id="navbarNav">
+                        <ul className="navbar-nav">
+                            <li className="nav-item active">
+                                <a className="nav-link">
+                                    <Nav.Link
+                                        as={NavLink}
+                                        exact
+                                        to="/"
+                                        activeStyle={{
+                                            fontWeight: "bold",
+                                            color: "black",
+                                        }}
+                                    >
+                                        PATH VISUALIZER
+                                    </Nav.Link>
+                                </a>
+                            </li>
+                            <li className="nav-item">
+                                <a className="nav-link">
+                                    <Nav.Link
+                                        as={NavLink}
+                                        exact
+                                        to="/theory"
+                                        activeStyle={{
+                                            fontWeight: "bold",
+                                            color: "black",
+                                        }}
+                                    >
+                                        GRAPH TRAVERSAL THEORY
+                                    </Nav.Link>
+                                </a>
+                            </li>
+                            <li className="nav-item">
+                                <a className="nav-link">
+                                    <Nav.Link
+                                        as={NavLink}
+                                        exact
+                                        to="/traversal"
+                                        activeStyle={{
+                                            fontWeight: "bold",
+                                            color: "black",
+                                        }}
+                                    >
+                                        HOW THE VISUALIZER WORKS
+                                    </Nav.Link>
+                                </a>
+                            </li>
+                        </ul>
+                    </div>
+                </nav>
+
+                {/* A <Switch> looks through its children <Route>s and
+            renders the first one that matches the current URL. */}
+                <Switch>
+                    <Route path="/theory">
+                        <Theory />
+                    </Route>
+                    <Route path="/regression">
+                        <Traversal />
+                    </Route>
+                    <Route path="/">
+                        <Graphs />
+                    </Route>
+                </Switch>
+            </div>
+        </Router>
+    );
 }
-
-export default App;
